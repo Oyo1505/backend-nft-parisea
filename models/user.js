174743fbfba038fa58cf591d1987dcd39ec56f0e
Melodie Shahrvari <mshahrvari@gmail.com>
2022-02-02 16:12:56 +0100

    User: model, seed, route

---
+++ models/user.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  image: {
+    type: String,
+    default:
+      "https://img.myloview.fr/stickers/default-avatar-profile-icon-vector-social-media-user-image-400-205124837.jpg",
+  },
+  coverImage: {
+    type: String,
+    default: "https://ansfl.org/wp-content/uploads/cover-default.png",
+  },
+  id_metamask: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  userName: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  following: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  follower: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  twitter: String,
+  facebook: String,
+  instagram: String,
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  nfts_ids_created: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  nfts_ids_owned: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  whishlist: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  bio: String,
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,69 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const userModel = require("../models/user");
+const uploader = require("../config/cloudinary ");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// DISPLAY USERS
+router.get("/users", async (req, res, next) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).json(users);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// EDIT - GET
+router.get("/users/:id", async (req, res, next) => {
+  try {
+    const singleUser = await userModel.findById(req.params.id);
+    res.status(200).json(singleUser);
+  } catch (e) {
+    next(e);
+  }
+}),
+
+// EDIT USER
+router.post("/users/edit/:id"),
+  uploader.single("image"),
+  uploader.single("coverImage"),
+  async (req, res, next) => {
+    try {
+      const {id} = req.params;
+      const {image, coverImage, name, userName, email, bio }
+      let newImage;
+      if (req.file) {
+        newImage = req.file.path;
+      } else {
+        newImage = image;
+      }
+
+      const editUser = await userModel.findByIdAndUpdate(
+        id,
+        {
+          image,
+          coverImage,
+          name,
+          userName,
+          email,
+          bio,
+        },
+        { new: true }
+      );
+      res.status(200).json(editUser);
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  // DELETE
+router.get("/users/:id", async (req, res) => {
+  try {
+    const userDelete = await userModel.findByIdAndDelete(req.params.id);
+    res.status(200).json(userDelete);
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
