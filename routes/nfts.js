594b5e00d3ea332aeee8dfa6e226f8b879558d55
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2022-02-04 06:44:34 +0100

    routes edit

--- routes/nfts.js
+++ routes/nfts.js
@@ -17,7 +17,7 @@ router.post("/nfts", async (req, res, next) => {
     const res = await nftModel.create(req.body);
     res.status(200).json(res);
   } catch (e) {
-    next(e);
+    next(e)
   }
 });
 
