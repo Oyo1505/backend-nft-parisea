27faafdf8e3ecf9fec6d27d86c5fcd4006296824
minniejung <94359917+minniejung@users.noreply.github.com>
2022-02-07 10:07:30 +0100

    post push

--- models/Post.model.js
+++ models/Post.model.js
@@ -2,20 +2,15 @@ const { model, Schema } = require("mongoose");
 
 const postSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: "user" },
-  userName: String,
-  userPfp: String,
   image: { type: String, required: true },
   description: String,
   postedTime: { type: Date, default: Date.now() },
-  likes: [{ type: Schema.Types.ObjectId, ref: "user" }],
   comments: [
     {
       userId: {
         type: Schema.Types.ObjectId,
         ref: "user",
       },
-      userName: String,
-      userPfp: String,
       comment: {
         type: String,
       },
