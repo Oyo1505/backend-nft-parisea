2748f72b75f64c1865b462bb4be418c2dedc84a0
Melodie Shahrvari <mshahrvari@gmail.com>
2022-02-13 17:06:31 +0100

    build

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 require("dotenv").config();
-require("./config/mongo"); 
+require("./config/mongo");
 var createError = require("http-errors");
 var express = require("express");
 var app = express();
 const cors = require("cors");
 app.use(express.json());
-const path = require("path");
-// 
-
-app.use(express.static(path.join(__dirname, "public/build")));
 
 const corsOptions = {
   origin: process.env.CLIENT_URL,
@@ -22,11 +18,11 @@ app.use(express.urlencoded({ extended: false }));
 // cors middle on
 app.use(cors(corsOptions));
 //BACK END
-// app.get("/", (req, res) => {
-//   res.send("backend server is running");
-// });
+app.get("/", (req, res) => {
+  res.send("backend server is running");
+});
 //------
-// ROUTESdd
+// ROUTES
 // ----
 const postsRouter = require("./routes/posts");
 const wishlistsRouter = require("./routes/wishlist");
@@ -45,11 +41,10 @@ app.use("/api/*", (req, res, next) => {
   error.status = 404;
   next(error);
 });
-
 if (process.env.NODE_ENV === "production") {
-  app.use("/*", (req, res, next) => {
+  app.use("*", (req, res, next) => {
     // If no routes match, send them the React HTML.
-    res.sendFile(path.join(__dirname, "public/build/index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
   });
 }
 // error handler
